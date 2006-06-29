ce06861b4c633967a586a2740010e859ae1e2a44
brent <brent@b5de89bc-dee3-0310-a7f3-e33a5ece582f>
2006-06-29 06:17:06 +0000

    adding new projects

---
+++ Debug/sources.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_UPPER_SRCS := 
+CPP_SRCS := 
+O_SRCS := 
+ASM_SRCS := 
+S_SRCS := 
+CXX_SRCS := 
+CC_SRCS := 
+OBJ_SRCS := 
+OBJS := 
+ARCHIVES := 
+CC_DEPS := 
+CPP_DEPS := 
+CXX_DEPS := 
+C_UPPER_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+
